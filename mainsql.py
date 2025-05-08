82172138a2678efccd0284218d540fef4aca1eb4
Dawood Ayaz <dawoodayaz18@gmail.com>
2025-05-08 23:43:33 +0500

    selecting and fetching data from database table with all rows and also specific row.

--- mainsql.py
+++ mainsql.py
@@ -17,28 +17,46 @@ if(mydb):
 
 mycursor = mydb.cursor()
 
+
 # To show databases created, we run this code:
 # mycursor.execute("SHOW DATABASES")
 # for db in mycursor:
 #     print(db)
 
+
 #TO CREATE A TABLE:
 # mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(10))")
 
+
 #To show a table created earlier
-mycursor.execute("SHOW TABLES")
+# mycursor.execute("SHOW TABLES")
+# for tb in mycursor:
+#     print(tb)
 
-for tb in mycursor:
-    print(tb)
 
-sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
-students = [("Bob", 22),
-            ("Bob", 18),
-            ("Bob", 28),
-            ("Bob", 32),
-            ("Bob", 25)]
+# sqlFormula = "INSERT INTO students (name, age) VALUES (%s, %s)"
+# students = [("Bob", 22),
+#             ("Bob", 18),
+#             ("Bob", 28),
+#             ("Bob", 32),
+#             ("Bob", 25)]
 # To execute a single string like ("Bob", 22), we use mycursor.execute
 # But for executing a tuple having arrays, we use mycursor.executemany.
-mycursor.executemany(sqlFormula, students)
+# mycursor.executemany(sqlFormula, students)
+# mydb.commit()
 
-mydb.commit()
+
+# Get all data in a table:
+# This code is for quering the database to select all records in a table and show them
+mycursor.execute("SELECT * FROM students")
+myresult = mycursor.fetchall()
+for row in myresult: 
+   print(row)
+
+
+# Get some specific data in the row of a table:
+# This command is for quering the database to select a specific record in a table and show on the screen:
+mycursor.execute("SELECT age FROM students")
+myresult = mycursor.fetchone()
+for row in myresult: 
+   print(row)
